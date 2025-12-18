742e37df65c4b79a8166682e6ffdb84a6944ce3d
Anderson Melo <142186594+AndersonMeloo@users.noreply.github.com>
2025-12-17 22:05:19 -0300

    feat(task.dto.ts) use validations dto

--- src/task/task.dto.ts
+++ src/task/task.dto.ts
@@ -1,9 +1,40 @@
+import {
+  IsDateString,
+  IsEnum,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export enum TaskStatusEnum {
+  TO_DO = 'TO_DO',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
+
 export class TaskDto {
+  @IsUUID()
+  @IsOptional()
   id: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(256)
   title: string;
+
+  @IsString()
+  @MinLength(5)
+  @MaxLength(512)
   description: string;
-  status: string;
-  expirationDate: Date;
+
+  @IsEnum(TaskStatusEnum)
+  @IsOptional()
+  status?: TaskStatusEnum;
+
+  @IsDateString()
+  expirationDate: string;
 }
 
 export class FindALLParameters {
